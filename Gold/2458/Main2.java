352fc952e3e88ebea29a5475826959d8560898dc
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-09-10 14:31:48 +0900

    키 순서
    플로이드 워셜

---
+++ Gold/2458/Main2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		boolean[][] adj = new boolean[n + 1][n + 1];
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			adj[a][b] = true; // a < b
+		}
+
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					if (adj[i][k] && adj[k][j]) adj[i][j] = true;
+				}
+			}				
+		}
+
+		int answer = 0;
+		for (int i = 1; i <= n; i++) {
+			int count = 0;
+			for (int j = 1; j <= n; j++) {
+				if (adj[i][j] || adj[j][i]) count++;
+			}
+			if (count == n - 1) answer++;
+		}				
+
+		System.out.println(answer);
+	}
+}
