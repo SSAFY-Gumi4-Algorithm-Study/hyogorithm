7821ec8d9a9b359b4a3d9ba5964c0a1d1eccb1b3
hjoo830 <105642658+hjoo830@users.noreply.github.com>
2025-11-06 20:48:52 +0900

    개수 세기

---
+++ Bronze/10807/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int v = Integer.parseInt(br.readLine());
+        System.out.println(map.getOrDefault(v, 0));
+    }
+}
